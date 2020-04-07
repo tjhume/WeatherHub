61c215a1b99f142223e513c7a4905207e7254a7e
tjhume <tristanjhume@gmail.com>
2020-04-07 17:55:32 -0600

    Home redirect correction again

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,12 @@
 var locations = Cookies.get('locations');
 if(locations){
-    window.location.replace("app/index.html");
+    var current = window.location.href;
+    current = current.replace('index.html', '');
+    if (current.substring(current.length-1) == "/"){
+        current = current.substring(0, current.length-1);
+    }
+    if (current.substring(current.length-1) == "/"){
+        current = current.substring(0, current.length-1);
+    }
+    window.location.replace(current + "/app/index.html");
 }
