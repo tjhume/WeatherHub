9149acdee5341392bb3890e91c6e5482f989393c
tjhume <tristanjhume@gmail.com>
2020-04-07 16:29:52 -0600

    Cookies and basic display working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,49 @@
-jQuery(document).ready(function($){
-    // If we are entering the page with a query from the home page
-    if(getLocation('location') != '' && getLocation('location') != undefined && getLocation('location').toLowerCase() != 'current location'){   
+var gettingCurrent = false;
+var gettingLocation = false;
 
+jQuery(document).ready(function($){
+    var location = getLocation('location');
+
+    var locations = Cookies.get('locations');
+    if(locations){
+        locations = locations.split(',');
+        for(var i = 0; i < locations.length; i++){
+            $('.location-select').append('<div class="location-box" role="button">'+location+'</div>');
+        }
     }
-    //If we are entering the page with no query
+
+    // If we are entering the page with a query from the home page
+    if(location != '' && location != undefined && location.toLowerCase() != 'current+location'){
+        getInfo(location);
+    }
+    // If we are entering the page with no query and no default location, use geolocation
     else{
-        location = 'Current Location';
         getCurrentLocation();
     }
+
+    $('.add-location .location-box').click(function(){
+        if(!gettingCurrent && !gettingLocation){
+            $('.form-overlay .error').html('');
+            $('.form-overlay').css('display', 'block');
+        }
+    });
+
+    $('#submit-button').click(function(){
+        var location = $('#search').val();
+        if(location == ''){
+            $('.form-overlay .error').html('Invalid input');
+        }else{
+            $('.loading').css('display', 'block');
+            $('.form-overlay').css('display', 'none');
+            getInfo(location);
+        }
+    })
 });
 
 
-//Functions
+// Functions
 
+// Get location from the URL
 function getLocation(sParam){
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -24,18 +55,112 @@ function getLocation(sParam){
     }
 }
 
+// Uses geolocation to get the current location
 function getCurrentLocation() {
+    gettingCurrent = true;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(geoSuccess, geoErr);
     } else {
-        $('#error').html('Geolocation is not supported on this browser');
+        gettingCurrent = false;
+        $('.loading').css('display', 'none');
+        alert('Geolocation is not supported on this browser.');
     }
 }
 
+// If geolocation succeeds we move over to the getInfoLatLon function
 function geoSuccess(pos){
-    alert('Successful geolocation!');
+    var request = $.get('https://api.openweathermap.org/data/2.5/weather?lat='+pos.coords.latitude+'&lon='+pos.coords.longitude+key);
+
+    request.done(function(data){
+        console.log(data);
+        getInfoLatLon(pos.coords.latitude, pos.coords.longitude, data.name, data.main.temp, data.main.feels_like, data.weather[0].description);
+    });
+
+    request.fail(function(){
+        gettingCurrent = false;
+        $('.loading').css('display', 'none');
+        alert('Something went wrong with geolocation');
+    });
 }
 
+// Alert if geolocation failes
 function geoErr(err){
+    gettingCurrent = false;
+    $('.loading').css('display', 'none');
     alert('Something went wrong with geolocation.');
+}
+
+var key = '&appid=34667b930b9d1bb7dfa30e0163f0e192';
+
+// How we normally display a location
+function getInfo(location){
+    gettingLocation = true;
+    var base = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    var url = base + location + key;
+    var request = $.get(url);
+
+    // Successful request
+    request.done(function(data){
+        var lon = data.coord.lon;
+        var lat = data.coord.lat;
+
+        //Move over to lat/long request
+        getInfoLatLon(lat, lon, location, data.main.temp, data.main.feels_like, data.weather[0].description);
+    });
+
+    // Alert if request failed
+    request.fail(function(){
+        gettingLocation = false;
+        gettingCurrent = false;
+        $('.loading').css('display', 'none');
+        alert('Something went wrong with your request!')
+    });
+}
+
+/**
+ * ALL SUCCESSFUL REQUESTS FOR LOCATION ARRIVE HERE
+ */
+function getInfoLatLon(lat, lon, location, currentTemp, currentFeels, currentDesc){
+    var url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+key;
+    var request = $.get(url);
+
+    // Successful location request
+    request.done(function(data){
+        var tomorrow = data.daily[1];
+        console.log(tomorrow);
+
+        var locations = [];
+        if(Cookies.get('locations')){
+            locations = Cookies.get('locations');
+            locations = locations.split(',');
+        }
+        if(!gettingCurrent && !locations.includes(location) && location.toLowerCase() != 'current location'){
+            locations.push(location);
+            $('.location-select').append('<div class="location-box" role="button">'+location+'</div>');
+            Cookies.set('locations', locations.toString(), { path: '/' });
+        }
+
+        $('.current-location-box').html('Location: ' + location);
+        $('.condition').html(currentDesc + ', ' + KtoF(currentTemp) + '&deg;');
+        $('.feels').html('Feels like: ' + KtoF(currentFeels) + '&deg;');
+        $('.tomorrow').html('Tomorrow: ' + tomorrow.weather[0].description + ' ' + KtoF(tomorrow.temp.day) + '&deg;');
+
+        gettingLocation = false;
+        gettingCurrent = false;
+        $('.loading').css('display', 'none');
+    });
+
+    // Failed location request
+    request.fail(function(){
+        gettingLocation = false;
+        gettingCurrent = false;
+        $('.loading').css('display', 'none');
+        alert('Something went wrong with your request!');
+    });
+}
+
+// Kelvin to Fahrenheit
+function KtoF(temp) {
+    temp = parseFloat(temp);
+    return Math.round(((temp-273.15)*1.8)+32);
 }
